fe858839d5c12f8c91e18df1552f098e4f6cf068
Ilyas <fortwink88@gmail.com>
2024-02-26 00:59:30 +0600

    user implementation

--- src/main/java/kz/aitu/airportmanagementsystem/repositories/UserRepositoryInterface.java
+++ src/main/java/kz/aitu/airportmanagementsystem/repositories/UserRepositoryInterface.java
@@ -4,8 +4,8 @@ import kz.aitu.airportmanagementsystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+    //main methods of user model
 public interface UserRepositoryInterface extends JpaRepository<User, Integer> {
     List<User> findByLastNameIgnoreCase(String lastName);
-    User findByPhoneNumber(String phoneNumber);
+    User    findByPhoneNumber(String phoneNumber);
 }
--- src/main/java/kz/aitu/airportmanagementsystem/services/interfaces/UserServiceInterface.java
+++ src/main/java/kz/aitu/airportmanagementsystem/services/interfaces/UserServiceInterface.java
@@ -3,7 +3,7 @@ package kz.aitu.airportmanagementsystem.services.interfaces;
 import kz.aitu.airportmanagementsystem.models.User;
 
 import java.util.List;
-
+    //main methods of user service
 public interface UserServiceInterface {
     List<User> getAll();
     User getById(int id);
