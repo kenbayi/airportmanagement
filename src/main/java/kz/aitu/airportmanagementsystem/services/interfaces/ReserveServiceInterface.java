da1da936af55804fea04f405369628909b3b8dce
Yerassyl <yerasil.yerkin@icloud.com>
2024-02-27 02:30:19 +0600

    Reservation implementation

--- src/main/java/kz/aitu/airportmanagementsystem/services/interfaces/ReserveServiceInterface.java
+++ src/main/java/kz/aitu/airportmanagementsystem/services/interfaces/ReserveServiceInterface.java
@@ -4,8 +4,15 @@ import kz.aitu.airportmanagementsystem.models.Reserve;
 
 import java.util.List;
 
+// Interface defining methods for managing flight reservations
 public interface ReserveServiceInterface {
+    // Method to retrieve all reservations
     List<Reserve> getAllReserve();
+
+    // Method to make a reservation based on flight ID and user phone number
     Reserve makeReservation(Long flightId, String phoneNumber);
+
+    // Method to view reservation history by reservation ID
     Reserve viewReservationHistory(int reserveId);
 }
+
