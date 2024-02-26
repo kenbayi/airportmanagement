674fcfc1b0c219734c225877252ace243847afb5
Yerassyl <yerasil.yerkin@icloud.com>
2024-02-27 02:29:15 +0600

    Reservation implementation

--- src/main/java/kz/aitu/airportmanagementsystem/repositories/ReserveRepositoryInterface.java
+++ src/main/java/kz/aitu/airportmanagementsystem/repositories/ReserveRepositoryInterface.java
@@ -3,7 +3,8 @@ package kz.aitu.airportmanagementsystem.repositories;
 import kz.aitu.airportmanagementsystem.models.Reserve;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Repository interface for managing Reserve entities, extending JpaRepository for basic CRUD operations
 public interface ReserveRepositoryInterface extends JpaRepository<Reserve, Integer> {
+    // Method to find a reservation by its ID
     Reserve findByReserveId(int reserveId);
 }
-
