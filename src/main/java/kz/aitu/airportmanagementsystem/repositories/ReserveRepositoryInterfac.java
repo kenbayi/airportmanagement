cf3ffb8c37a4eedc1b060423f3089abb5c32471e
Yerassyl <yerasil.yerkin@icloud.com>
2024-02-27 02:25:16 +0600

    fixed bug

--- src/main/java/kz/aitu/airportmanagementsystem/repositories/ReserveRepositoryInterfac.java
+++
@@ -1,8 +0,0 @@
-package kz.aitu.airportmanagementsystem.repositories;
-
-import kz.aitu.airportmanagementsystem.models.Reserve;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReserveRepositoryInterface extends JpaRepository<Reserve, Integer> {
-    Reserve findByReserveId(int reserveId);
-}
